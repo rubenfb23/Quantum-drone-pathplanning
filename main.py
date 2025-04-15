f53aa10660cd8e8be951d5aa5c16ae600521e142
Rubén Fernández Boullón <rubenfernandezboullon@gmail.com>
2025-04-15 11:10:34 +0200

    Added main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ This file initializes the quantum algorithm and handles high-level orchestration
 
 from services.path_planning_service import PathPlanningService
 
+
 def main():
     """Main function to execute the quantum path-planning algorithm."""
     service = PathPlanningService()
     service.execute()
+
 
 if __name__ == "__main__":
     main()
